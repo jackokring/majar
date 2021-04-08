4b734ab5a1d0f5160583de689ddee7a9fd3ca6a4
jackokring <jackokring@gmail.com>
2021-04-08 17:26:10 +0100

    ColoUrs.

--- src/uk/co/kring/Main.java
+++ src/uk/co/kring/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         for(int i = 0; i < args.length; i++) {
-            print(args[0]);
+            print(ANSI_GREEN + args[0]);
             execute(args);
         }
         println();
@@ -20,12 +20,22 @@ public class Main {
         return true;//default OK
     }
 
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_BLACK = "\u001B[30m";
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_YELLOW = "\u001B[33m";
+    public static final String ANSI_BLUE = "\u001B[34m";
+    public static final String ANSI_PURPLE = "\u001B[35m";
+    public static final String ANSI_CYAN = "\u001B[36m";
+    public static final String ANSI_WHITE = "\u001B[37m";
+
     public static void print(String s) {
         if(s == null) return;
         System.out.print(s+" ");
     }
 
     public static void println() {
-        System.out.println();
+        System.out.println(ANSI_RESET);
     }
 }
