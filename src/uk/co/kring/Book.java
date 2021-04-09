dfb9651af4abee59c0466422de760bace18984f5
jackokring <jackokring@gmail.com>
2021-04-09 18:31:13 +0100

    Books of symbols.

--- src/uk/co/kring/Book.java
+++ src/uk/co/kring/Book.java
@@ -2,7 +2,7 @@ package uk.co.kring;
 
 public class Book extends Symbol {
 
-    public Book(Book b, String name, String[] s) {
-        super(b, name, s);
+    public Book(Book b, String name) {
+        super(b, name, new String[0]);//empty to start
     }
 }
