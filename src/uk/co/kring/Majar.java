8bc05fa7f37072b8b2ed184e74360e02b936e9c5
jackokring <jackokring@gmail.com>
2021-04-11 11:38:44 +0100

    Refactor find.

---
+++ src/uk/co/kring/Majar.java
@@ -0,0 +1,13 @@
+package uk.co.kring;
+
+public class Majar extends Prim {
+
+    public Majar() {
+        super("majar");
+    }
+
+    public void def() {
+        //TODO
+        //language loader
+    }
+}
