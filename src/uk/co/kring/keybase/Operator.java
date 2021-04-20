def4d6883f2396f590ac05684221d3e4369c2a38
jackokring <jackokring@gmail.com>
2021-04-20 13:56:50 +0100

    Data.

---
+++ src/uk/co/kring/keybase/Operator.java
@@ -0,0 +1,17 @@
+package uk.co.kring.keybase;
+
+/**
+ * A class representing a CRUD on a key.
+ */
+public abstract class Operator {
+
+    Key key;
+
+    /**
+     * Create a new operator.
+     * @param key the key to operate on.
+     */
+    public Operator(Key key) {
+
+    }
+}
