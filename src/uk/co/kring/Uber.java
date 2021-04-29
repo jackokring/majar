c2c6392f442de9a901ed26f1335017c18f6aa3e4
jackokring <jackokring@gmail.com>
2021-04-29 14:47:49 +0100

    Uber.

--- src/uk/co/kring/Uber.java
+++ src/uk/co/kring/Uber.java
@@ -13,9 +13,22 @@ public abstract class Uber extends AbstractMultex {
         super(s);
     }
 
+    /**
+     * Obtain an uber of the correct class conversion.
+     * @param clazz the kind of class to convert into.
+     * @return another uber of the required class or a fail null.
+     */
+    public abstract Uber convert(Class<? extends Uber> clazz);
+
     protected boolean listBasis() {
         return false;
     }
 
+    /**
+     * Gets the multex representation if listing of the basis makes no sense without
+     * manufacturing it. This maybe because it is inefficient to keep converting it.
+     * If you wish to effect a null, just make listBasis return false.
+     * @return an effective conversion. Must not return null.
+     */
     protected abstract String[] getBasis();
 }
