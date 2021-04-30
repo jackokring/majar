b31eddaa337510e4d083bd7d0617257270e8fe4d
jackokring <jackokring@gmail.com>
2021-04-30 20:07:25 +0100

    A profound decision.

--- src/uk/co/kring/Book.java
+++ src/uk/co/kring/Book.java
@@ -21,6 +21,14 @@ public class Book extends UnitSymbol {
             //placed here things are faster but a not found error replaces
             //other errors and has an increased risk of running a word in a namespace collision
             //a better compromise
+            //The Parallel Book Hypothesis
+            //Within the meaning of other books signalled via name lies a fail, an attempt a not found
+            //A re-context and a request for sourcing a meaning.
+            //Execution of other thread's words maybe bad, and so is stopped by not letting
+            //the book into context.
+            //the words go on, and try though, maybe having a better meaning within self.
+            //This is not just a speed consideration, it is an optimization of the
+            //application of filling in new found words to become the selective source omni.
             m.setError(Main.ERR_FOR_CON, this);
         } else {
             m.switchContext(this);
